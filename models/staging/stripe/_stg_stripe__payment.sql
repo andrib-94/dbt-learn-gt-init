85b8d2d97838895c81d45e78c725b943eac37016
Andrzej Borowiec <76168946+andrib-94@users.noreply.github.com>
2026-08-29 15:22:16 +0200

    updated payment staging model

--- models/staging/stripe/_stg_stripe__payment.sql
+++ models/staging/stripe/_stg_stripe__payment.sql
@@ -7,11 +7,11 @@ with source as (
 renamed as (
 
     select
-        id,
-        orderid,
-        paymentmethod,
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
         status,
-        amount,
+        amount / 100 as amount,
         created,
         _batched_at
 
